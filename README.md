bb297805e240d7b5bfb21bd7320e28cac6b8f3fb
Kamas 'Iceberg' Lau <kamaslau@users.noreply.github.com>
2024-11-11 00:30:02 +0800

    feat: simple authorization control

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # kuaidi100-sdk
-快递100
+
+快递 100
+
+## Usage
+
+创建并配置 _.env_ 文件
+
+```bash
+cp .env.sample .env
+```
+
+```text
+PORT=3000
+TOKEN=CHANGE_ME # 访问凭证，简单的权限控制
+KEY=YOUR_KUAIDI100_KEY # 快递100的 key
+SECRET=YOUR_KUAIDI100_SECRET # 快递100的 secret
+
+```
+
+启动服务
+
+```bash
+# 开发环境
+pnpm start:dev
+# 生产、测试环境
+pnpm start
+```
--- src/app.ts
+++ src/app.ts
@@ -10,7 +10,23 @@ const app = new Koa()
 app.on('error', globalErrorHandler)
 
 app.use(async (ctx, next) => {
-  // console.log(ctx.path)
+  if (String(process.env.TOKEN)?.length < 10) {
+    ctx.status = 503
+    ctx.body = { message: 'Service Unavailable; Server config has not been completed yet' }
+
+    if (process.env.NODE_ENV !== 'production') console.error('Change TOKEN value in .env file first')
+
+  } else if (ctx.query?.token !== process.env.TOKEN) {
+    ctx.status = 401
+    ctx.body = { message: 'Unauthorized' }
+
+  } else {
+    await next()
+  }
+})
+
+app.use(async (ctx, next) => {
+  console.log(ctx.path)
 
   try {
     if (ctx.path === '/questPrice') {
@@ -26,6 +42,8 @@ app.use(async (ctx, next) => {
   } catch (error) {
     ctx.status = 500
     ctx.body = { message: (error as Error)?.message }
+
+    return
   }
 })
 
