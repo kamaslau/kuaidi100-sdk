57c9d4766568c787c949eca25978c48b1f8e0b13
Kamas Lau <kamaslau@outlook.com>
2025-09-19 12:26:03 +0800

    refactor: remove jest

--- src/libs/utils.ts
+++ src/libs/utils.ts
@@ -1,5 +1,6 @@
 import path from "node:path"
 import { fileURLToPath } from "node:url"
+import Koa from 'koa'
 
 const filePath = fileURLToPath(import.meta.url) // THIS file
 const fileDirPath = path.dirname(filePath)
@@ -8,7 +9,7 @@ export const BASE_PATH = path.join(fileDirPath, '../') // root dir
 /**
  * 错误处理
  */
-export const globalErrorHandler = (error: Error, ctx) => {
+export const globalErrorHandler = (error: Error, ctx: Koa.Context) => {
   console.error('server error: ', (error as Error)?.message)
 }
 
